187789e6055315ea32d2e348abbbf8dc705f3cc2
Lee Scott <leewilliamscott@protonmail.com>
2024-11-20 19:02:09 +0000

    Working Convergence
    Needs:
    -Comments
    -Possible arrange results
    -Likelihood update
    -Remove overlaps on combined_data
    -Correct return format for final result

--- R/combine.R
+++ R/combine.R
@@ -1,95 +1,69 @@
 library(tidyverse)
 
-#Initialisation - Narayan
+#Initialisation - Narayan & Lee
 initialise <- function(data) {
-  initialisation <- list()
-  # extract the mean lengths in each age group from the frogs with known ages
-  meanLengths <- data %>%
-    group_by(Age) %>%
-    summarise(meanLength = mean(Length)) %>%
-    select(meanLength) %>%
-    unlist()
+  known <- data[!is.na(data$Age), ]
+  unknown <- data[is.na(data$Age), ]
+  
+  age_groups <- sort(unique(known$Age))
+  k <- length(age_groups)
+  rows <- nrow(data)
+  urows <- length(unknown$Age)
+  
+  mu_known <- tapply(known$Length, known$Age, mean)
 
-  # using these means, generate bounds to initialise the age groups with
-  group1Cutoff <- (meanLengths[1] + meanLengths[2])/2
-  group2Cutoff <- (meanLengths[2] + meanLengths[3])/2
-
-  # mutate the Age column based on these initial bounds
-  data <- data %>%
-    mutate(Age = if_else(!is.na(Age), Age, # if the entry already has an value, don't mutate it
-                         # if the entry is currently NA, change it in accordance with my bounds
-                         if_else(0<=Length & Length<=group1Cutoff, 1, # if Length is within group1Cutoff, place it in age group 1
-                                 if_else(group1Cutoff<Length & Length<=group2Cutoff, 2, # if length is within group1 and group2Cutoff, place it in age group 2
-                                         if_else(group2Cutoff<Length, 3, Age))))) # if length is greater than group2Cutoff, place it in age group 3
-
-  # summarise the initialisation
-  initDataSummary <- data %>%
-    group_by(Age) %>%
-    summarise(meanLength = mean(Length),
-              sdLength = sd(Length))
-
-
-  # extract the initial parameter values from this summary
-  muHat <- pull(initDataSummary[,2]) # pull the second column from initialise
-  sigmaHat <- pull(initDataSummary[,3]) # pull the third row from initialise
-  lambdaHat <- as.vector(table(data$Age) / nrow(data)) # compute the lambdas as proportions
-
-  return(list(
-    muHat = muHat,
-    sigmaHat = sigmaHat,
-    lambdaHat = lambdaHat
-  ))
+  for (i in 1:urows) {
+    unknown$Age[i] <- age_groups[which.min(abs(unknown$Length[i] - mu_known))]
+  }
+  
+  combined_data <- rbind(known, unknown)
+  
+  mu <- tapply(combined_data$Length, combined_data$Age, mean)
+  sigma <- tapply(combined_data$Length, combined_data$Age, sd)
+  lambda <- as.numeric(table(combined_data$Age)/nrow(combined_data))
+  
+  inits <- data.frame(mu, sigma, lambda)
+  rownames(inits) <- c("Age1", "Age2", "Age3")
+  
+  return (inits)
 }
 
 #Expectation - Lee
-expector <- function(data, initialisation) {
-  # Creating subsets of data set for groups with known/ unknown ages
+expector <- function(data, estimates) {
   known <- data[!is.na(data$Age), ]
   unknown <- data[is.na(data$Age), ]
 
-  # Combining known and unknown data (after filled) for full data set
   combined_data <- rbind(known, unknown)
-
-  # Initialising information to be used throughout function
+  
   age_groups <- sort(unique(known$Age))
   k <- length(age_groups)
   rows <- nrow(data)
 
-  # Creating empty data frame to be filled with densities
-  densities <- data.frame(matrix(0, nrow = nrow(data), ncol = k))
+  densities <- data.frame(matrix(0, nrow = rows, ncol = k))
   colnames(densities) <- c("Age1", "Age2", "Age3")
 
-  # Using initial guess as first estimate
-  estimates <- initialisation
-
-  # Calculating densities for each yi for each age group with Gaussian probability density function
-  # based off estimates for mu and sigma for unknown age groups
   for (i in 1:rows) {
 
     yi <- data$Length[i]
 
-    densities[i, ] <- c(dnorm(yi, mean = estimates$muHat[1], sd = estimates$sigmaHat[1]),
-                        dnorm(yi, mean = estimates$muHat[2], sd = estimates$sigmaHat[2]),
-                        dnorm(yi, mean = estimates$muHat[3], sd = estimates$sigmaHat[3]))
+    densities[i, ] <- c(dnorm(yi, mean = estimates$mu[1], sd = estimates$sigma[1]),
+                        dnorm(yi, mean = estimates$mu[2], sd = estimates$sigma[2]),
+                        dnorm(yi, mean = estimates$mu[3], sd = estimates$sigma[3]))
   }
 
-  # Creating empty data frame to store posteriors in
   posteriors <- data.frame(matrix(0, nrow = nrow(data), ncol = k))
   colnames(posteriors) <- c("Age1", "Age2", "Age3")
 
-  # Calculating posteriors for each yi for each age group for unknown age groups
   for (i in ((length(known$Age) + 1):rows)) {
 
     yi <- combined_data$Length[i]
-    densities_yi <- densities[i, ] # Getting densities for specific yi
+    densities_yi <- densities[i, ]
 
-    # Calculating total probability
     Pyi <-0
     for (j in 1:k) {
       Pyi <- Pyi + densities_yi[j]*estimates$lambda[j]
     }
 
-    # Calculating and storing posteriors using Bayes Theorem
     for (l in 1:k) {
       posteriors[i, l] <- (densities_yi[l]*estimates$lambda[l])/Pyi
     }
@@ -105,146 +79,124 @@ expector <- function(data, initialisation) {
   ))
 }
 
-#Maximisation - Yi
+#
 maximiser <- function(data, posteriors) {
-  # Variables to be used later in the function
-  k <- ncol(posteriors) # Number of groups (age classes)
-  N <- nrow(data)       # Number of data points
+  known <- data[!is.na(data$Age), ]
+  unknown <- data[is.na(data$Age), ]
+  
+  data <- rbind(known, unknown)
+  
+  k <- ncol(posteriors)
+  N <- nrow(data)
 
-  # Initialize updated parameter estimates
-  muHat <- c()
-  sigmaHat <- c()
-  lambdaHat <- c()
+  mu <- c()
+  sigma <- c()
+  lambda <- c()
 
-  # For each group, update the parameters
   for (j in 1:k) {
-    # Extract posterior probabilities for group j
     P_ij <- posteriors[, j]
 
-    # Update mu_k
-    muHat <- c(muHat, sum(P_ij * data$Length) / sum(P_ij))
+    mu <- c(mu, sum(P_ij * data$Length) / sum(P_ij))
 
-    # Update sigma_k
-    sigmaHat <- c(sigmaHat, sqrt(sum(P_ij * (data$Length - muHat[j])^2) / sum(P_ij)))
+    sigma <- c(sigma, sqrt(sum(P_ij * (data$Length - mu[j])^2) / sum(P_ij)))
 
-    # Update lambda_k
-    lambdaHat <- c(lambdaHat, sum(P_ij) / N)
+    lambda <- c(lambda, sum(P_ij) / N)
   }
-
-  return(list(
-    muHat = muHat,
-    sigmaHat = sigmaHat,
-    lambdaHat = lambdaHat
-  ))
+  
+  estimates <- data.frame(mu, sigma, lambda)
+  rownames(estimates) <- c("Age1", "Age2", "Age3")
+  
+  return(estimates)
 }
 
-#Checking convergence - Ivor
-
-#Checking convergence of a specific point
-checkConvergence <- function(logLikelihoods, iterations, epsilon = 1e-08) {
-  #Must immediately break
+#
+checkConvergence <- function(logLikelihoods, iterations, epsilon) {
+  
   minIterations <- 2
   if(iterations < minIterations) {
     return(FALSE)
   }
 
-  #If change is too small for tolerance, function has converged
   change <- abs(logLikelihoods[iterations] - logLikelihoods[iterations - 1])
+  if (iterations %% 100 == 0) {
+    print(change)
+  }
   changeBelowTolerance <- change < epsilon
 
   return(changeBelowTolerance)
 }
 
-#Computes log likelihoods of current estimate and densities
+#
 findLogLikelihood <- function(data, densities, estimates) {
+  known <- data[!is.na(data$Age), ]
+  unknown <- data[is.na(data$Age), ]
+  
+  data <- rbind(known, unknown)
+  
+  likelihood <- 0
+  
   N <- nrow(data)
-  K <- length(estimates$muHat)
-  
-  logLikelihoods <- numeric(N)
-  
-  for(i in 1:N){
-    likelihood <- 0
-    #Sum likelihoods across all age groups
+  K <- length(estimates$mu)
+
+  for(n in 1:N){
+    a <- 0
+
     for(k in 1:K) {
-      likelihood <- likelihood + estimates$lambdaHat[k] * densities[i, k]
+      a <- a + estimates$lambda[k] * densities[n, k]
     }
     
-    if(likelihood <= 0) {
-      likelihood <- .Machine$double.eps
+    likelihood <- likelihood + log(a)
     }
-    
-    logLikelihoods[i] <- log(likelihood)
-  }
   
-  logLikelihood <- sum(logLikelihoods)
-  return(logLikelihood)
+  return(likelihood)
 }
 
-#Implements the loop to estimate parameters until either converge or max iterations
-converger <- function(data, initialisation, epsilon, maxit) {
+#
+converger <- function(data, inits, epsilon, maxit) {
+  known <- data[!is.na(data$Age), ]
+  unknown <- data[is.na(data$Age), ]
+  
+  data <- rbind(known, unknown)
 
-  #Iteration 0: posteriors and densities are using initialised variables
   iterations <- 0
-  estimates <- NULL
-  estimates <- initialisation
+  estimates <- inits
   logLikelihoods <- numeric(maxit)
   converged <- FALSE
 
   while (!converged && iterations < maxit) {
     iterations <- iterations + 1
-    #Expectation: update posterior probabilities and densities
     expectations <- expector(data, estimates)
-    #Maximisation: update parameter estimates
     estimates <- maximiser(data, expectations$posteriors)
-    #check if convergence is reached
     logLikelihoods[iterations] <- findLogLikelihood(data, expectations$densities, estimates)
     converged <- checkConvergence(logLikelihoods, iterations, epsilon)
   }
 
   return(list(
     iterations = iterations,
-    expectations = expectations,
     estimates = estimates,
+    expectations = expectations,
     logLikelihoods = logLikelihoods,
     converged = converged
   ))
 }
 
-#Arranges results to be in required format
-arrangeResult <- function(initialisation, convergence) {
-  estimates <- data.frame(
-    mu = convergence$estimates$muHat,
-    sigma = convergence$estimates$sigmaHat,
-    lambda = convergence$estimates$lambdaHat
-  )
-  rownames(estimates) <- c("Age1", "Age2", "Age3")
+inits <- initialise(x)
+convergence <- converger(x, inits, 1e-08, 1000)
+results <- (list(convergence$estimates,
+             inits,
+             convergence$converged,
+             convergence$expectations$posteriors,
+             convergence$logLikelihoods))
 
-  inits <- data.frame(
-    mu = initialisation$muHat,
-    sigma = initialisation$sigmaHat,
-    lambda = initialisation$lambdaHat
-  )
-  rownames(estimates) <- c("Age1", "Age2", "Age3")
 
-  converged <- convergence$converged
-  posterior <- convergence$expectations$posteriors
-  likelihood <- convergence$logLikelihoods
-  return(list(
-    estimates = estimates,
-    inits = inits,
-    converged = converged,
-    posterior = posterior,
-    likelihood = likelihood
-  ))
-}
-
-#Main function
-teamEM <- function(data, epsilon = 1e-08, maxit = 1000) {
-  initialisation <- initialise(data)
-  convergence <- converger(data, initialisation, epsilon, maxit)
-  result <- arrangeResult(initialisation, convergence)
-  return(result)
-}
-
-load("FishLengths.RData")
-result <- teamEM(x)
+#teamEM <- function(data, epsilon = 1e-08, maxit = 1000) {
+#  inits <- initialise(data)
+#  convergence <- converger(data, inits, epsilon, maxit)
+#  return (list(convergence$estimates,
+#               inits,
+#               convergence$converged,
+#               convergence$expectations$posteriors,
+#               convergence$logLikelihoods))
+#}
+#
+#result <- teamEM(x)
