6d263081355600160ca3bceff1c2541bfc4a9bba
109north <nam27@st-andrews.ac.uk>
2024-11-22 13:00:37 +0000

    Add all comments

--- R/combine.R
+++ R/combine.R
@@ -29,49 +29,51 @@ initialise <- function(data) {
   return (inits)
 }
 
-#Expectation - Lee
+##### Expectation - Lee #####
 expector <- function(data, estimates) {
-  known <- data[!is.na(data$Age), ]
-  unknown <- data[is.na(data$Age), ]
+  known <- data[!is.na(data$Age), ] # known values are not NA values
+  unknown <- data[is.na(data$Age), ] # unknown values are NA values
 
-  combined_data <- rbind(known, unknown)
+  combined_data <- rbind(known, unknown) #combine data
 
   age_groups <- sort(unique(known$Age))
-  k <- length(age_groups)
+  k <- length(age_groups) # amount of age groups
   rows <- nrow(data)
 
-  densities <- data.frame(matrix(0, nrow = rows, ncol = k))
+  densities <- data.frame(matrix(0, nrow = rows, ncol = k)) # initialise densities object
   colnames(densities) <- c("Age1", "Age2", "Age3")
 
   for (i in 1:rows) {
 
-    yi <- data$Length[i]
+    yi <- data$Length[i] # observed length i
 
-    densities[i, ] <- c(dnorm(yi, mean = estimates$mu[1], sd = estimates$sigma[1]),
-                        dnorm(yi, mean = estimates$mu[2], sd = estimates$sigma[2]),
-                        dnorm(yi, mean = estimates$mu[3], sd = estimates$sigma[3]))
+    densities[i, ] <- c(dnorm(yi, mean = estimates$mu[1], sd = estimates$sigma[1]), # Gaussian pdf with mu and sd of Age 1
+                        dnorm(yi, mean = estimates$mu[2], sd = estimates$sigma[2]), # Gaussian pdf with mu and sd of Age 2
+                        dnorm(yi, mean = estimates$mu[3], sd = estimates$sigma[3])) # Gaussian pdf with mu and sd of Age 3
   }
 
-  posteriors <- data.frame(matrix(0, nrow = nrow(data), ncol = k))
+  posteriors <- data.frame(matrix(0, nrow = nrow(data), ncol = k)) # initialise posteriors object
   colnames(posteriors) <- c("Age1", "Age2", "Age3")
 
-  for (i in ((length(known$Age) + 1):rows)) {
+  for (i in ((length(known$Age) + 1):rows)) { # iterating through the unknown data only
 
-    yi <- combined_data$Length[i]
-    densities_yi <- densities[i, ]
+    yi <- combined_data$Length[i] # observed length
+    densities_yi <- densities[i, ] # observed Gaussian pdf
 
+    # sum across each age group the product of the density and the lambda value
     Pyi <-0
     for (j in 1:k) {
       Pyi <- Pyi + densities_yi[j]*estimates$lambda[j]
     }
 
+    # calculate the posterior probility of the length belonging to each age group, entering into the posteriors object accordingly
     for (l in 1:k) {
       posteriors[i, l] <- (densities_yi[l]*estimates$lambda[l])/Pyi
     }
   }
 
-  for (i in 1:(length(known$Age))) {
-    posteriors[i, known$Age[i]] <- 1
+  for (i in 1:(length(known$Age))) { # iterating through the known values
+    posteriors[i, known$Age[i]] <- 1 # the posterior probability of the known values is 1 since we already know it
   }
 
   return(list(
@@ -80,12 +82,12 @@ expector <- function(data, estimates) {
   ))
 }
 
-#
+##### Maximisation - Yi #####
 maximiser <- function(data, posteriors) {
-  known <- data[!is.na(data$Age), ]
-  unknown <- data[is.na(data$Age), ]
+  known <- data[!is.na(data$Age), ] # known values are not NA values
+  unknown <- data[is.na(data$Age), ] # unknown values are NA values
 
-  data <- rbind(known, unknown)
+  data <- rbind(known, unknown) # combine data
 
   k <- ncol(posteriors)
   N <- nrow(data)
@@ -95,58 +97,56 @@ maximiser <- function(data, posteriors) {
   lambda <- c()
 
   for (j in 1:k) {
-    P_ij <- posteriors[, j]
+    P_ij <- posteriors[, j] # posterior probabilities for age group j
 
-    mu <- c(mu, sum(P_ij * data$Length) / sum(P_ij))
-
-    sigma <- c(sigma, sqrt(sum(P_ij * (data$Length - mu[j])^2) / sum(P_ij)))
-
-    lambda <- c(lambda, sum(P_ij) / N)
+    mu <- c(mu, sum(P_ij * data$Length) / sum(P_ij)) # compute updated mu estimate as defined in algorithm
+    sigma <- c(sigma, sqrt(sum(P_ij * (data$Length - mu[j])^2) / sum(P_ij))) # compute sd estimate as defined in algorithm
+    lambda <- c(lambda, sum(P_ij) / N) # compute lambda estimate as defined in algorithm
   }
 
-  estimates <- data.frame(mu, sigma, lambda)
+  estimates <- data.frame(mu, sigma, lambda) # update estimates object with this maximisation
   rownames(estimates) <- c("Age1", "Age2", "Age3")
 
   return(estimates)
 }
 
-#
+##### Convergence - Ivor #####
 checkConvergence <- function(logLikelihoods, iterations, epsilon) {
 
-  minIterations <- 2
+  minIterations <- 2 # minimum 2 iterations so that the change criteria runs
   if(iterations < minIterations) {
     return(FALSE)
   }
 
-  change <- abs(logLikelihoods[iterations] - logLikelihoods[iterations - 1])
-  if (iterations %% 100 == 0) {
-    print(change)
-  }
-  changeBelowTolerance <- change < epsilon
+  change <- abs(logLikelihoods[iterations] - logLikelihoods[iterations - 1]) # calculate the change in log likelihood from the last iteration
+  #if (iterations %% 100 == 0) {
+  #  print(change)
+  #}
+  changeBelowTolerance <- change < epsilon # check if the change is below tolerance
 
-  return(changeBelowTolerance)
+  return(changeBelowTolerance) # if returns TRUE, function has converged
 }
 
-#
-findLogLikelihood <- function(data, densities, estimates) {
-  known <- data[!is.na(data$Age), ]
-  unknown <- data[is.na(data$Age), ]
 
-  data <- rbind(known, unknown)
+findLogLikelihood <- function(data, densities, estimates) {
+  known <- data[!is.na(data$Age), ] # known values are not NA
+  unknown <- data[is.na(data$Age), ] # unknown values are NA
+
+  data <- rbind(known, unknown) # combine data
 
   likelihood <- 0
 
   N <- nrow(data)
-  K <- length(estimates$mu)
+  K <- length(estimates$mu) # amount of age groups
 
-  for(n in 1:N){
+  for(n in 1:N){ # iterate through the rows of the data
     a <- 0
 
-    for(k in 1:K) {
-      a <- a + estimates$lambda[k] * densities[n, k]
+    for(k in 1:K) { # iterate through each age group
+      a <- a + estimates$lambda[k] * densities[n, k] # add the product of the lambda estimate and density for that age group
     }
 
-    likelihood <- likelihood + log(a)
+    likelihood <- likelihood + log(a) # add the log of this value to the overall likelihood, since the log of a product is a sum of logs.
     }
 
   return(likelihood)
@@ -154,22 +154,23 @@ findLogLikelihood <- function(data, densities, estimates) {
 
 #
 converger <- function(data, inits, epsilon, maxit) {
-  known <- data[!is.na(data$Age), ]
-  unknown <- data[is.na(data$Age), ]
+  known <- data[!is.na(data$Age), ] # known data is not NA
+  unknown <- data[is.na(data$Age), ] # unknown data is NA
 
-  data <- rbind(known, unknown)
+  data <- rbind(known, unknown) # combine data
 
+  # initialise all variables for the loop
   iterations <- 0
-  estimates <- inits
+  estimates <- inits # initialise estimates to data initialisation
   logLikelihoods <- numeric(maxit)
   converged <- FALSE
 
-  while (!converged && iterations < maxit) {
+  while (!converged && iterations < maxit) { # while not converged and within max iterations
     iterations <- iterations + 1
-    expectations <- expector(data, estimates)
-    estimates <- maximiser(data, expectations$posteriors)
-    logLikelihoods[iterations] <- findLogLikelihood(data, expectations$densities, estimates)
-    converged <- checkConvergence(logLikelihoods, iterations, epsilon)
+    expectations <- expector(data, estimates) # complete expectation step
+    estimates <- maximiser(data, expectations$posteriors) # complete maximisation step
+    logLikelihoods[iterations] <- findLogLikelihood(data, expectations$densities, estimates) # compute loglikelihood for this maximisation
+    converged <- checkConvergence(logLikelihoods, iterations, epsilon) # check if function converged based on previous loglikelihood value
   }
 
   return(list(
@@ -181,23 +182,17 @@ converger <- function(data, inits, epsilon, maxit) {
   ))
 }
 
-inits <- initialise(x)
-convergence <- converger(x, inits, 1e-08, 1000)
-results <- (list(convergence$estimates,
-             inits,
-             convergence$converged,
-             convergence$expectations$posteriors,
-             convergence$logLikelihoods))
 
+##### Run Algorithm #####
+teamEM <- function(data, epsilon = 1e-08, maxit = 1000) {
+  inits <- initialise(data)
+  convergence <- converger(data, inits, epsilon, maxit)
+  return (list(convergence$estimates,
+               inits,
+               convergence$converged,
+               convergence$expectations$posteriors,
+               convergence$logLikelihoods))
+}
 
-#teamEM <- function(data, epsilon = 1e-08, maxit = 1000) {
-#  inits <- initialise(data)
-#  convergence <- converger(data, inits, epsilon, maxit)
-#  return (list(convergence$estimates,
-#               inits,
-#               convergence$converged,
-#               convergence$expectations$posteriors,
-#               convergence$logLikelihoods))
-#}
-#
-#result <- teamEM(x)
+load("FishLengths.RData")
+result <- teamEM(x)
