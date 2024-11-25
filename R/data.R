422b02e45a7d79f248e5f5b4c8fa37808a24bdc1
109north <nam27@st-andrews.ac.uk>
2024-11-25 23:33:12 +0000

    Dataset documentation and reformat

---
+++ R/data.R
@@ -0,0 +1,31 @@
+#' Fish Lengths Data
+#'
+#' Data of fish of different lengths and their associated age group. 100 observations contain an age group assignments, 900 observations contain an unassigned age group.
+#' Report ...
+#'
+#' @format ## `FishLengths`
+#' A data frame with 1000 observations on the following 3 variables.
+#' \describe{
+#'   \item{FishID}{a numeric vector}
+#'   \item{Length}{a numeric vector}
+#'   \item{Age}{a numeric vector}
+#'   ...
+#' }
+#' @source <https://moody.st-andrews.ac.uk/moodle/mod/resource/view.php?id=1267766>
+"x"
+
+#' Documentation Example Data
+#'
+#' Fake data used to verify examples in function documentation
+#' Report ...
+#'
+#' @format ## `docExampleData`
+#' A csv with 6 observations on the following 3 variables.
+#' \describe{
+#'   \item{FishID}{a numeric vector}
+#'   \item{Length}{a numeric vector}
+#'   \item{Age}{a numeric vector}
+#'   ...
+#' }
+#' @source generateDataset function
+"docExampleData"
