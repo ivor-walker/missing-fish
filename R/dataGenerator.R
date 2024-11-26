e80f5ced5a3d8263d6cdf41c2d27c535f232dceb
Yi Zhang <Yi-Zh17@outlook.com>
2024-11-26 11:11:45 +0000

    fixed typos

--- R/dataGenerator.R
+++ R/dataGenerator.R
@@ -15,7 +15,7 @@ library(dplyr)
 #' known <- data[!is.na(data$Age), ] # known data is not NA
 #' unknown <- data[is.na(data$Age), ] # unknown data is NA
 #' sorted_data <- rbind(known, unknown) # combine data
-#' inits <- initialise(knwon, unknown, sorted_data)
+#' inits <- initialise(known, unknown, sorted_data)
 #'
 #' generateDataset(0, 100, 1000, inits, c(0.9, 0.04, 0.03, 0.03))
 generateDataset <- function(seed, fish, datasize, inits, distributions) {
@@ -26,13 +26,13 @@ generateDataset <- function(seed, fish, datasize, inits, distributions) {
   # sample different values for Age with replacement with probability specified in `distributions`
   Age <- sample(c(NA, 1, 2, 3), size = datasize, replace = TRUE, prob = distributions)
 
-  lengths <- numeric(datasize) # initialize lengths colummn
+  lengths <- numeric(datasize) # initialize lengths column
 
   for (i in 1:datasize) {
     if (is.na(Age[i])) { # if Age is NA for that observation
       # take a random value for length with a random mean and sd from the intialisation of FishLengths Dataset
       lengths[i] <- rnorm(1, mean = sample(inits$mu, 1), sd = sample(inits$sigma, 1))
-      # else take a random value for length in accordane with the mean and sd from that given Age group in the initialisation of FishLengths Dataset
+      # else take a random value for length in accordance with the mean and sd from that given Age group in the initialisation of FishLengths Dataset
     } else {
       lengths[i] <- rnorm(1, mean = inits$mu[Age[i]], sd = inits$sigma[Age[i]])
     }
